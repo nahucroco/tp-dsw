c6c32f84832d91bb2a60938705ead5aaa1765737
Nahuel <nahueluriel2013@gmail.com>
2025-08-01 18:30:22 -0300

    Ultimas actualizaciones primera entrega

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log(Servidor corriendo en http://localhost:${PORT});
+app.listen(3000, () => {
+	console.log(`Servidor corriendo en http://localhost:3000`);
 });
