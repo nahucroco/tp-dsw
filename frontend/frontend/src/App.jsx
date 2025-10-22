8c451bc43394fbc113ddfc844a0f18a4d391c8f6
nahucroco <nahueluriel2013@gmail.com>
2025-10-22 18:06:29 -0300

    Creacion proyecto react vacío

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Biblioteca DSW</h1>
+      <p className="text-center">Frontend React funcionando correctamente 🚀</p>
+    </div>
+  );
+}
+
+export default App;
