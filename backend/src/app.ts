e87d19be35e428bbf96b35e4705afa988cdc13a4
lucio <lucioalba02@gmail.com>
2025-10-28 19:49:15 -0300

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,19 +6,40 @@ import authorRoutes from './routes/authorRoutes.js';
 import bookCopyRoutes from './routes/bookCopyRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
 import genderRoutes from './routes/genderRoutes.js';
-
+import cors from 'cors';
 const app = express();
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			const allowedOrigins = ['http://localhost:5173'];
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error('Not allowed by CORS'));
+			}
+		},
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+	}),
+);
 
 app.use(express.json());
-app.use((_req, _res, next) => {
-	RequestContext.create(orm.em, next);
-});
+app.use(
+	(
+		_req: express.Request,
+		_res: express.Response,
+		next: express.NextFunction,
+	) => {
+		RequestContext.create(orm.em, next);
+	},
+);
 await syncSchema(); // only in dev;
 app.use('/api/books', bookRoutes);
 app.use('/api/authors', authorRoutes);
 app.use('/api/genders', genderRoutes);
 app.use('/api/book_copies', bookCopyRoutes);
-app.use((_, res) => {
+app.use((_req: express.Request, res: express.Response) => {
 	return res.status(404).send({ message: 'Not Found' });
 });
 export default app;
